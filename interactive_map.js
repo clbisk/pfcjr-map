f6440f27f6198ac8e2ab0050b0442280bbebfff2
Cecilia Bisk <cecilia.bisk.17@cnu.edu>
2021-10-28 14:32:26 +0900

    infinite yucky boy popups

--- interactive_map.js
+++ interactive_map.js
@@ -1,111 +1,19 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+var stateList = [].slice.call(document.querySelectorAll('.state'));
+var popoverHandleList = [].slice.call(document.querySelectorAll('.popover-handle'));
+
+stateList.map(state => {
+	new bootstrap.Tooltip(state, {
+		placement: 'auto',  
+		title: state.getAttribute("state-name")       
+	})
 });
 
-$(function () {
-	$('.example-popover').popover({
-		container: 'body'
-	})
-})
+popoverHandleList.map(popoverHandle => {
+	console.log(popoverHandle.getAttribute("state-name"));
 
-//Southern states
-	$("#TX").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-	$("#AL").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-	$("#AR").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  	$("#DE").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#FL").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#GA").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#KY").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#LA").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#MD").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#MS").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#OK").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#NC").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#SC").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#TN").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#WV").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-  $("#VA").click(function(){window.open("https://pfcjreform.org/southern-states/","_blank");});
-  
-//Western states
-    
-  $("#AZ").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  $("#CA").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  $("#CO").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-    
-  $("#ID").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  $("#MT").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  $("#NV").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  $("#NM").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  $("#OR").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  $("#UT").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  $("#WA").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  $("#WY").click(function(){window.open("https://pfcjreform.org/western-states/","_blank");});
-  
-  //MidWestern States
-  
-  $("#IL").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#IN").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#KS").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#IA").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#MI").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#MO").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#MN").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#NE").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#ND").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#OH").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#SD").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  $("#WI").click(function(){window.open("https://pfcjreform.org/midwestern-states/","_blank");});
-  
-  //Northeastern states
-  
-  $("#CT").click(function(){window.open("https://pfcjreform.org/northeastern-states/","_blank");});
-  
-  $("#ME").click(function(){window.open("https://pfcjreform.org/northeastern-states/","_blank");});
-  
-  
-  $("#NH").click(function(){window.open("https://pfcjreform.org/northeastern-states/","_blank");});
-  
-  $("#MA").click(function(){window.open("https://pfcjreform.org/northeastern-states/","_blank");});
-  
-  $("#NJ").click(function(){window.open("https://pfcjreform.org/northeastern-states/","_blank");});
-  
-  $("#PA").click(function(){window.open("https://pfcjreform.org/northeastern-states/","_blank");});
-  
-  $("#RI").click(function(){window.open("https://pfcjreform.org/northeastern-states/","_blank");});
-  
-  $("#NY").click(function(){window.open("https://pfcjreform.org/northeastern-states/","_blank");});
-  
-  $("#VT").click(function(){window.open("https://pfcjreform.org/northeastern-states/","_blank");});
+	new bootstrap.Popover(popoverHandle, {    
+		content: popoverHandle.getAttribute("state-policy"),
+		title: popoverHandle.getAttribute("state-name"),
+		placement: 'auto'
+	});
+});
--- register_map_widget.php
+++ register_map_widget.php
@@ -1,8 +1,10 @@
 <?php
 /*
 Plugin Name: Interactive Map for pfcjreform.org
-Description: an interactive policy map created by Heewon Park
- */
+Description: an interactive policy map designed by Heewon Park and implemented by Cecilia Bisk
+Author: Cecilia Bisk, Heewon Park
+Version: 1.0
+*/
 
 namespace Interactive_Policy_Map;
 
