12c932ba3fdfc898a9b123e3183ee8367d6b68f4
Cecilia Bisk <cecilia.bisk.17@cnu.edu>
2021-11-08 16:42:03 +0900

    experimenting with timings

--- add_links_to_text.js
+++ add_links_to_text.js
@@ -1,5 +1,27 @@
+jQuery(document).ready(function ($) {
+	// URL Modal
+	var urlModal = '<div class=\'target-link modal fade\' id="target-link-modal-' + uid + '" tabindex="-1" role="dialog" aria-hidden="true">' +
+		'	<div class="modal-dialog" role="document"><div class="modal-content">' +
+		'		<div class="modal-header">' +
+		'			<div>Input Link URL</div>' +
+		'		</div>' +
+		'		<div class="modal-body">' +
+		'			<input class="target-link-input" placeholder=\'www.example.com\' type=\'text\' aria-label=\'link URL\'/>' +
+		'		</div>' +
+		'		<div class="modal-footer">' +
+		'			<button type="button" class="btn btn-primary btn-sm">Create Link</button>' +
+		'		</div>' +
+		'	</div></div>' +
+		'</div>';
+
+	$('body').append(urlModal);
+	console.log('url modal added');
+	console.log(urlModal);
+});
+
+
 function openLinkCreator() {
-	jQuery(document).ready(function($) {
+	jQuery(document).ready(function ($) {
 		console.log(".link-button click registered");
 		$('.link-button button').attr('disabled', true);
 		$('.target-link button').attr('disabled', false);
@@ -9,26 +31,50 @@ function openLinkCreator() {
 
 var textWithLinksView = elementor.modules.controls.BaseData.extend({
 	onReady: function () {
-		var highlighted = "";
+		var highlighted = null;
+		var uid = this.get_control_uid();
+
+		jQuery(document).ready(function ($) {
+			// URL Modal
+			var urlModal = '<div class=\'target-link modal fade\' id="target-link-modal-' + uid + '" tabindex="-1" role="dialog" aria-hidden="true">' +
+				'	<div class="modal-dialog" role="document"><div class="modal-content">' +
+				'		<div class="modal-header">' +
+				'			<div>Input Link URL</div>' +
+				'		</div>' +
+				'		<div class="modal-body">' +
+				'			<input class="target-link-input" placeholder=\'www.example.com\' type=\'text\' aria-label=\'link URL\'/>' +
+				'		</div>' +
+				'		<div class="modal-footer">' +
+				'			<button type="button" class="btn btn-primary btn-sm">Create Link</button>' +
+				'		</div>' +
+				'	</div></div>' +
+				'</div>';
+
+			$('body').append(urlModal);
+			console.log('url modal added');
+			console.log(urlModal);
+		});
 	},
 
 	saveHighlighted: function () {
-		jQuery(document).ready(function($) {
+		jQuery(document).ready(function ($) {
+			console.log("saving highlighted");
+
 			if (window.getSelection) {
 				this.highlighted = window.getSelection();
 			} else if (document.selection && document.selection.type != "Control") {
 				this.highlighted = document.selection.createRange();
 			} else {
-				this.highlighted = "";
+				this.highlighted = null;
 			}
-	
+
 			highlightedText = highlighted.getText();
 			console.log(highlightedText);
 		});
 	},
 
 	saveValue: function () {
-		jQuery(document).ready(function($) {
+		jQuery(document).ready(function ($) {
 			var descriptionText = $('.elementor-control-tag-area').val();
 			this.setValue(descriptionText);
 		});
--- text_with_links_control.php
+++ text_with_links_control.php
@@ -11,7 +11,7 @@ class Text_With_Links_Control extends Base_Data_Control {
 
 	public function enqueue() {
 		// styles
-		wp_enqueue_style('textwithlinks', plugins_url('textwithlinks.css', __FILE__), array(), '2.2.1');
+		wp_enqueue_style('textwithlinks', plugins_url('textwithlinks.css', __FILE__), array(), '2.2.2.1');
 
 		wp_enqueue_style('bootstrap-css', '//cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
 		
@@ -22,7 +22,7 @@ class Text_With_Links_Control extends Base_Data_Control {
 
 		$my_script_dependencies = array('jquery', 'bootstrap');
 		wp_enqueue_script('add_links_to_text', plugins_url('add_links_to_text.js', __FILE__),
-		$my_script_dependencies, '2.10', true);
+		$my_script_dependencies, '2.10.1.1', true);
 	}
 
 	protected function get_default_settings() {
@@ -35,16 +35,13 @@ class Text_With_Links_Control extends Base_Data_Control {
 	public function content_template() {
 		$control_uid = $this->get_control_uid();
 
-		$link_modal = 
-		$link_popover = '"tomato"';
-
 		echo '<div class="elementor-control-field">';
 
 		echo '  <div class="toolbar">';
 		echo '      <label for="' . esc_attr( $control_uid ) . '" class = "elementor-control-title">{{{ data.label }}}</label>';
 
-		echo '		<div class="link-button" onclick="saveHighlighted()">';
-		echo '			<button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#target-link-modal">';
+		echo '		<div class="link-button">';
+		echo '			<button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#target-link-modal-' . esc_attr( $control_uid ) . '">';
 		echo '				<i class="fas fa-link fa-xs"></i>';
 		echo '          </button>';
 		echo '      </div>';
@@ -56,21 +53,6 @@ class Text_With_Links_Control extends Base_Data_Control {
 		echo '      </textarea>';
 		echo '  </div>';
 
-		echo '</div>';
-
-		// URL Modal
-		echo '<div class=\'target-link modal fade\' id="target-link-modal" tabindex="-1" role="dialog" aria-hidden="true">';
-		echo '	<div class="modal-dialog" role="document"><div class="modal-content">';
-		echo '		<div class="modal-header">';
-		echo '			<div>Input Link URL</div>';
-		echo '		</div>';
-		echo '		<div class="modal-body">';
-		echo '			<input class="target-link-input" placeholder=\'www.example.com\' type=\'text\' aria-label=\'link URL\'/>';
-		echo '		</div>';
-		echo '		<div class="modal-footer">';
-		echo '			<button type="button" class="btn btn-primary btn-sm" onclick=\'createLink()\'>Create Link</button>';
-		echo '		</div>';
-		echo '	</div></div>';
 		echo '</div>';
 	}
 }
